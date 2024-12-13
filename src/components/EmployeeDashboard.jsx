3c5a2c5e0d1115cb6322dace3b620b2b1bba7b07
ShreyaSingh6377 <100568092+ShreyaSingh6377@users.noreply.github.com>
2024-12-13 11:07:34 +0530

    Update EmployeeDashboard.jsx

--- src/components/EmployeeDashboard.jsx
+++ src/components/EmployeeDashboard.jsx
@@ -35,7 +35,7 @@ const EmployeeDashboard = () => {
                     </button>
                 </div>
                 <ul className="mt-6">
-                    {/* View Profile Link */}
+                  
                     <li
                         onClick={() => setActiveDashboard('view_profile_emp')}
                         className={`px-6 py-3 hover:bg-gray-800 cursor-pointer flex items-center transition-colors duration-200 ease-in-out ${activeDashboard === 'view_profile_emp' ? 'bg-gray-800' : ''
@@ -44,7 +44,7 @@ const EmployeeDashboard = () => {
                         <FaUser className="mr-3 text-xl" /> View Profile
                     </li>
 
-                    {/* Edit Profile Link */}
+                   
                     <li
                         onClick={() => setActiveDashboard('update_profile_emp')}
                         className={`px-6 py-3 hover:bg-gray-800 cursor-pointer flex items-center transition-colors duration-200 ease-in-out ${activeDashboard === 'update_profile_emp' ? 'bg-gray-800' : ''
@@ -53,7 +53,7 @@ const EmployeeDashboard = () => {
                         <FaEdit className="mr-3 text-xl" /> Edit Details
                     </li>
 
-                    {/* Submit Leave Request Link */}
+                    
                     <li
                         onClick={() => setActiveDashboard('submit_leave_req')}
                         className={`px-6 py-3 hover:bg-gray-800 cursor-pointer flex items-center transition-colors duration-200 ease-in-out ${activeDashboard === 'submit_leave_req' ? 'bg-gray-800' : ''
@@ -62,7 +62,7 @@ const EmployeeDashboard = () => {
                         <FaLeaf className="mr-3 text-xl" /> Submit Leave Request
                     </li>
 
-                    {/* View All Leaves Link */}
+                    
                     <li
                         onClick={() => setActiveDashboard('getallleaves')}
                         className={`px-6 py-3 hover:bg-gray-800 cursor-pointer flex items-center transition-colors duration-200 ease-in-out ${activeDashboard === 'getallleaves' ? 'bg-gray-800' : ''
@@ -71,7 +71,7 @@ const EmployeeDashboard = () => {
                         <FaCalendarAlt className="mr-3 text-xl" /> See All Leaves
                     </li>
 
-                    {/* Get Payroll */}
+                   
                     <li
                         onClick={() => setActiveDashboard('getpayroll')}
                         className={`px-6 py-3 hover:bg-gray-800 cursor-pointer flex items-center transition-colors duration-200 ease-in-out ${activeDashboard === 'getpayroll' ? 'bg-gray-800' : ''
@@ -83,28 +83,28 @@ const EmployeeDashboard = () => {
                 <Logout />
             </div>
 
-            {/* Main Content */}
+            
             <div className="flex-1 bg-gray-100 p-6 overflow-hidden transition-all duration-300 ease-in-out">
-                {/* View Profile */}
+                
                 {activeDashboard === 'view_profile_emp' && <Employee />}
 
-                {/* Update Profile */}
+               
                 {activeDashboard === 'update_profile_emp' && <EmployeeUpdate />}
 
-                {/* Submit Leave Request */}
+                
                 {activeDashboard === 'submit_leave_req' && <LeaveRequest />}
 
-                {/* Get All Leaves */}
+               
                 {activeDashboard === 'getallleaves' && <GetAllLeaves />}
 
-                {/* Get Payroll */}
+                
                 {activeDashboard === 'getpayroll' && <EmployeePayroll />}
             </div>
 
-            {/* Holiday Calendar Modal */}
+            
             {isCalendarModalOpen && (
                 <HolidayCalendarModal
-                    onClose={() => setIsCalendarModalOpen(false)} // Close calendar modal
+                    onClose={() => setIsCalendarModalOpen(false)} 
                 />
             )}
         </div>
